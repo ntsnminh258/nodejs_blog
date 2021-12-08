00743f1107bc909d140cd3c0de37b7529ea07f4a
ntsnminh258 <ntsnminh258@gmail.com>
2021-12-08 23:08:23 +0700

    OK

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController {
+  index(req, res) {
+    res.render("news");
+  }
+
+  //   GET /news/:slug
+  show(req, res) {
+    res.send("NEW DETAILS");
+  }
+}
+
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -6,20 +6,29 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan("combined"));
+// Middleware
+// Xừ lý gừi từ html
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Xừ lý gừi từ JS
+app.use(express.json());
+
+// HTTP Logger
+// app.use(morgan("combined"));
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./src/resources/views");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
